78f6304eb93fcd58778ba12246edaa6696d28f85
Patrick Allaert <patrick@catchy.io>
2017-01-27 13:01:06 +0100

    Changed: using persistent connexions

--- config-dev/db-connection.php
+++ config-dev/db-connection.php
@@ -1,3 +1,3 @@
 <?php
 
-return new PDO("mysql:host=localhost;dbname=sample", "sampleuser", "samplepass");
+return new PDO("mysql:host=localhost;dbname=sample", "sampleuser", "samplepass", [PDO::ATTR_PERSISTENT => true]);

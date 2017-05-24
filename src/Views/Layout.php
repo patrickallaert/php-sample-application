ed9fa2e8bae08d7a8390966339bb0da133db11e4
Patrick Allaert <patrick@catchy.io>
2017-05-24 22:56:37 +0200

    Changed: incorporating bootstrap

--- src/Views/Layout.php
+++ src/Views/Layout.php
@@ -23,7 +23,7 @@ class Layout
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet prefetch" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet prefetch" href="/css/bootstrap.min.css">
   <link rel="stylesheet" href="/css/twitter.css">
 
   <title><?= htmlspecialchars($this->title) ?></title>

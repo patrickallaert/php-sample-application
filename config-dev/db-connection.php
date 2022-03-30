ec7ce9b8b8ff656a871c5cb3412c0434b2be80b5
iuribe18 <ivanuribegonzalez@gmail.com>
2022-03-30 00:14:59 -0500

    Creación docker-compose app y db con mysql

--- config-dev/db-connection.php
+++ config-dev/db-connection.php
@@ -1,3 +1,3 @@
 <?php
 
-return new PDO("mysql:host=localhost;dbname=sample", "sampleuser", "samplepass", [PDO::ATTR_PERSISTENT => true]);
+return new PDO("mysql:host=mysql-clouxter;dbname=sample", "sampleuser", "samplepass", [PDO::ATTR_PERSISTENT => true]);
